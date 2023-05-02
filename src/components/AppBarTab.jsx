36846cfd89f5bfe202a285e5ea94d9b78fd617ea
Keshab Manni <34715906+keshabmanni@users.noreply.github.com>
2023-05-02 11:27:32 +0530

    10.4 - Update (AppBar Tab as Pressable)

--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,11 +1,12 @@
+import { Pressable } from "react-native";
 import Text from "./Text";
 import theme from "./Theme";
 
 const AppBarTab = ({ tab }) => {
     return (
-        <Text fontWeight="bold" color="primary" style={{ backgroundColor: theme.colors.appBarBG, padding: 20 }}>
-            {tab}
-        </Text>
+        <Pressable>
+            <Text color='primary' style={{ backgroundColor: theme.colors.appBarBG, padding: 20 }}>{tab}</Text>
+        </Pressable>
     )
 }
 
