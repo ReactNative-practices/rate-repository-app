923d2602dbbe1c7918b44512bfc73192e42e6308
Keshab Manni <34715906+keshabmanni@users.noreply.github.com>
2023-05-07 19:20:55 +0530

    Enhancing Apollo Client's requests

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client";
-import Constants from "expo-constants";
 
 import Main from './src/components/Main';
 import createApolloClient from "./src/utils/apolloClient";
+import AuthStorage from "./src/utils/authStorage";
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
-  console.log("env:", Constants.manifest.extra.env);
   return (
     <>
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
-          <Main />
         </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" />
